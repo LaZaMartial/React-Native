85a397edc0d42e263904359f4e2b85777c36c563
LaZaMartial <martialandrihertse@gmail.com>
2025-04-08 12:28:31 +0300

    config appwrite

--- services/appwrite.tsx
+++ services/appwrite.tsx
@@ -1,10 +1,25 @@
 // appwrite.js
-import { Client } from "appwrite";
+import { Client, Databases } from "appwrite";
+
+const config = {
+  endpoint: "https://cloud.appwrite.io/v1",
+  projectId: "67e3bf2900125bf93147",
+  db: "67e3c0f8000f4cbcffb0",
+  collectionId: "67e3c1e5003adaae1104",
+  col: {
+    mat: "mat",
+    nom: "nom",
+    note_mat: "note_mat",
+    note_phy: "note_phy",
+  },
+};
 
 const client = new Client();
 
 client
   .setEndpoint("https://cloud.appwrite.io/v1") // Remplacez par votre endpoint Appwrite
   .setProject("67e3bf2900125bf93147"); // Remplacez par votre ID de projet
+
+const database = new Databases(client);
 
 export default client;
